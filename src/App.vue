d4b972ee9033b3b33122ccc9cbf35f98c7fea65c
Gabriel Kostik <gabrielkostik@gmail.com>
2023-08-22 21:30:48 -0300

    Adicionado arquivos

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="calculator">
+    <h1>Calculadora Aritmética</h1>
+    <div>
+      <input v-model="num1" type="number" placeholder="Número 1" @input="calculate" />
+      <select v-model="operator" @change="calculate">
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+      </select>
+      <input v-model="num2" type="number" placeholder="Número 2" @input="calculate" />
+      <div class="result">{{ result }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      operator: "+",
+      result: 0,
+    };
+  },
+  methods: {
+    calculate() {
+      switch (this.operator) {
+        case "+":
+          this.result = this.num1 + this.num2;
+          break;
+        case "-":
+          this.result = this.num1 - this.num2;
+          break;
+        case "*":
+          this.result = this.num1 * this.num2;
+          break;
+        case "/":
+          this.result = this.num1 / this.num2;
+          break;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.calculator {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+}
+
+input,
+select {
+  margin: 10px;
+  padding: 5px;
+}
+
+.result {
+  margin-top: 20px;
+  font-size: 24px;
+}
+</style>
